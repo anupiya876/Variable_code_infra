fea8ab5022219ae36ad57620c41785aefe7289c4
anupama singh <anupiyarockstar1998@gmail.com>
2025-08-10 23:11:04 +0530

    master commit

--- Environment/Dev/provider.tf
+++ Environment/Dev/provider.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name = ""
+    storage_account_name = ""
+    container_name = ""
+    key = ""
+  }
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
@@ -9,5 +15,5 @@ terraform {
 
 provider "azurerm" {
 features {}
-subscription_id = ""
+subscription_id = "5fbead97-423e-4814-809c-114db991d911"
 }
