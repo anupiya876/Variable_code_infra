e79954cc8370f68b714b9a0f0b3d923d631ff89e
anupama singh <anupiyarockstar1998@gmail.com>
2025-08-10 22:40:32 +0530

    added in feature branch

--- Environment/Dev/provider.tf
+++ Environment/Dev/provider.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "azurerm"{
+    resource_group_name = "anu_workrg"
+    storage_account_name = "anustgaccount"
+    container_name = "anubackendcontainer"
+    key = "anu.terraform.statefile"
+  }
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
